94911a7a2484037322f71ec7652dbc574ae27e3f
tom-alcorn <tom@adharmonics.com>
2015-03-27 15:59:37 -0400

    Update cli.py

--- flask_server/cli.py
+++ flask_server/cli.py
@@ -11,19 +11,7 @@ def get_image(url):
 
 if __name__ == '__main__':
     """Tool to test the raw output of pytesseract with a given input URL"""
-    sys.stdout.write("""
-===OOOO=====CCCCC===RRRRRR=====\n
-==OO==OO===CC=======RR===RR====\n
-==OO==OO===CC=======RR===RR====\n
-==OO==OO===CC=======RRRRRR=====\n
-==OO==OO===CC=======RR==RR=====\n
-==OO==OO===CC=======RR== RR====\n
-===OOOO=====CCCCC===RR====RR===\n\n
-""")
     sys.stdout.write("A simple OCR utility\n")
     url = raw_input("What is the url of the image you would like to analyze?\n")
     image = get_image(url)
-    sys.stdout.write("The raw output from tesseract with no processing is:\n\n")
-    sys.stdout.write("-----------------BEGIN-----------------\n")
-    sys.stdout.write(pytesseract.image_to_string(image) + "\n")
-    sys.stdout.write("------------------END------------------\n")
+    sys.stdout.write("\n" + pytesseract.image_to_string(image) + "\n")
