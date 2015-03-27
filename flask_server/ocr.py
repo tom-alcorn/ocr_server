d55905d536a249ce0abdafc3e6d21b9084662fe7
tom-alcorn <tom@adharmonics.com>
2015-03-27 15:58:45 -0400

    Removed image.filter step

--- flask_server/ocr.py
+++ flask_server/ocr.py
@@ -7,7 +7,7 @@ from StringIO import StringIO
 
 def process_image(url):
     image = _get_image(url)
-    image.filter(ImageFilter.SHARPEN)
+    # image.filter(ImageFilter.SHARPEN)
     return pytesseract.image_to_string(image)
 
 
