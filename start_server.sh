6f66fdb2b35866a3380daa997c78ebd3a193cff5
Tom Alcorn <tom@adharmonics.com>
2015-04-02 15:16:19 -0400

    added kill -9 command to remove pre-existing server processes

--- start_server.sh
+++ start_server.sh
@@ -5,6 +5,8 @@
 LOGFILE=/home/apprunner/ocr/ocr_server/start_server.log
 touch $LOGFILE
 
+kill -9 `ps -ef | grep ocr_server/flask_server/app.py | grep -v grep | awk '{print $2}'`
+
 nohup python /home/apprunner/ocr/ocr_server/flask_server/app.py > $LOGFILE 2>&1 &
 
 exit
