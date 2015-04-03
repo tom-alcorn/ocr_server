10ef40fbbeab654610a0f2b80052fc317cf3f263
Tom Alcorn <tom@adharmonics.com>
2015-04-03 15:43:57 -0400

    fixed deployment of server in start_server and caught IOErrors thrown by pytesseract

--- flask_server/ocr.py
+++ flask_server/ocr.py
@@ -8,7 +8,13 @@ from StringIO import StringIO
 def process_image(url):
     image = _get_image(url)
     # image.filter(ImageFilter.SHARPEN)
-    return pytesseract.image_to_string(image)
+    try:
+        text = pytesseract.image_to_string(image)
+    except IOError as e:
+        print "Warning IOError with url %s" % url
+        print e
+        text = ""
+    return text
 
 
 def _get_image(url):
